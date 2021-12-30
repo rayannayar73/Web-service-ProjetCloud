4ef217cdeb86b0ea47f09789b4c429d8a84fc4d8
rayan73 <rayannayar73@gmail.com>
2021-12-30 14:24:38 +0300

    first commit
    mandefa ny template backoffice

---
+++ src/test/java/projetCloud/WebServiceProjetCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package projetCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjetCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
