6558e88df249d18d27b464437ad864173b570f35
rayan73 <rayannayar73@gmail.com>
2022-01-17 21:03:16 +0300

    Update Region.java

--- src/main/java/projetCloud/model/Region.java
+++ src/main/java/projetCloud/model/Region.java
@@ -16,16 +16,14 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+	@Column(name = "nom")
+    private String nom;
+	
     public Region(long id, String nom) {
 		super();
 		this.id = id;
 		this.nom = nom;
 	}
-    public Region() {}
-	@Column(name = "nom")
-    private String nom;
-    
-    
     
     public Region() {
 		super();
