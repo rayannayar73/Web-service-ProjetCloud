944a2cba54afef48f5b21ec0b0045eedcda88978
rayan73 <rayannayar73@gmail.com>
2022-01-31 22:28:01 +0300

    Revert "Merge pull request #34 from rayannayar73/dev"
    This reverts commit f4fdb132567f8a0a62d0f9dc76766fb01efe5b1c, reversing
    changes made to 1f64051d10d563f6f65f29f1d0d370fbb041473a.

--- src/main/java/projetCloud/model/Utilisateur.java
+++ src/main/java/projetCloud/model/Utilisateur.java
@@ -21,12 +21,6 @@ public class Utilisateur {
 	@Column(name = "prenom", nullable = false)
 	private String prenom;
 	
-    @Column(name = "email", nullable = false)
-	private String email;	
-	
-	@Column(name = "mdp", nullable = false)
-	private String mdp;
-	
 	public Utilisateur() {
 		super();
 	}
@@ -36,12 +30,10 @@ public class Utilisateur {
 		this.id = id;
 	}
 
-	public Utilisateur(String nom, String prenom, String email, String mdp) {
+	public Utilisateur(String nom, String prenom) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
-        this.email = email;
-		this.mdp = mdp;
 	}
 	
 	public long getId() {
@@ -67,23 +59,6 @@ public class Utilisateur {
 
 	@Override
 	public String toString() {
-		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom +" email="+email +" mdp="+ mdp + "]";
+		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + "]";
 	}
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMdp() {
-        return mdp;
-    }
-
-    public void setMdp(String mdp) {
-        this.mdp = mdp;
-    }
-
 }
--- src/main/java/projetCloud/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package projetCloud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import projetCloud.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>{
-
-}
